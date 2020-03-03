0c0a5f3ae186eae4f99373380af5d745a352f6fc
EunSeok <eunsuk93@naver.com>
2020-03-03 16:41:57 +0900

    2020/03/03

--- D4/1865_동철이의 일 분배.py
+++ D4/1865_동철이의 일 분배.py
@@ -4,14 +4,14 @@ def johab(idx,end,result):
         if result > max_result:
             max_result = result
         return
-    if result <= max_result:
+    if result <= max_result: # 가지치기 중간까지의 결과값이 max보다 작으면 앞으로도 가능성 없음 // 곱하는 값들이 1보다 작기떄문에
         return 
     for i in range(N):
-        if not visit[i]:
-            new_result = result * probability[idx][i] * 0.01
-            visit[i] = 1
+        if not visit[i]: # 방문 하지 않았다면 == 일이 선택되지 않았다면
+            new_result = result * probability[idx][i] * 0.01 
+            visit[i] = 1 
             johab(idx+1,end,new_result)
-            visit[i] = 0
+            visit[i] = 0 # return 되면서 방문 배열 초기화
         
 T = int(input())
 for t in range(T):
@@ -19,7 +19,7 @@ for t in range(T):
     probability = []
     for _ in range(N):
         probability.append(list(map(int,input().split())))
-    visit =[0 for _ in range(N)]
+    visit =[0 for _ in range(N)] # 방문 배열 == 일
     max_result = -99999999
     johab(0,N,1)
     print("#{} {}".format(t+1,format(max_result*100,".6f")))
