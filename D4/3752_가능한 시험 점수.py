f0bf463233655a41f9077d45dd1c5aed045779d5
EunSeok <eunsuk93@naver.com>
2020-03-05 20:12:03 +0900

    2020/03/05

--- D4/3752_가능한 시험 점수.py
+++ D4/3752_가능한 시험 점수.py
@@ -2,11 +2,11 @@ T = int(input())
 for t in range(T):
     N = int(input())
     score = list(map(int,input().split()))
-    score_visit = [1] + [0] * sum(score)
-    score2 = [0]
-    for point in score:
-        for i in range(len(score2)):
-            if not score_visit[point+score2[i]]:
-                score_visit[point+score2[i]] = 1
-                score2.append(point+score2[i])
+    score_visit = [1] + [0] * sum(score) # 0 점은 반드시 존재
+    score2 = [0] 
+    for point in score: # 점수를 뽑아서 
+        for i in range(len(score2)): # 결과와 더해 나간다.
+            if not score_visit[point+score2[i]]: # 더한 값이 방문한적 없으면 중복이 아니기에 
+                score_visit[point+score2[i]] = 1 # 방문 배열에 체크하고
+                score2.append(point+score2[i]) # 결과에 더해준다
     print('#{} {}'.format(t+1,len(score2)))
