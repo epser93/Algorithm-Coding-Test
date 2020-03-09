742a322c864e82696def92c9c7b8dadad98a3de5
Song Eun Seok <eunsuk93@naver.com>
2020-03-09 13:05:51 +0900

    2020/03/09

--- D4/1238_Contact.py
+++ D4/1238_Contact.py
@@ -1,7 +1,7 @@
-def bfs(start_node):
+def bfs(start_node): # BFS 탐색 단계별로 진행함으로서 마지막 연락을 알 수 있음
     next_queue = [start_node]
     while 1:
-        queue = next_queue[:]
+        queue = next_queue[:] # 단계별로 진행시키기 위해 복사 큐를 만듬
         temp_queue = queue[:]
         next_queue = []
         while queue:
@@ -10,7 +10,7 @@ def bfs(start_node):
                 for node in contact[q]:
                     if not visit[node]:
                         visit[node] = 1
-                        next_queue.append(node)
+                        next_queue.append(node) # 다음 단계 큐에 append
         if len(next_queue) == 0:
             return max(temp_queue)
             
@@ -19,11 +19,11 @@ for t in range(10):
     number = list(map(int,input().split()))
     visit = [0] * (max(number)+1)
     contact ={}
-    for i in range(0,len(number),2):
+    for i in range(0,len(number),2): # 경로 딕셔너리로 저장
         if number[i] not in contact:
             contact[number[i]] = [number[i+1]]
         else:
-            if number[i+1] not in contact[number[i]]:
+            if number[i+1] not in contact[number[i]]: # 중복 방지
                 contact[number[i]].append(number[i+1])
     visit[start] = 1
     print('#{} {}'.format(t+1,bfs(start)))
