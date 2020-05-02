d85c95e076974a1c77b841a12e911fc29fc36e4b
Song Eun Seok <eunsuk93@naver.com>
2020-05-02 17:13:08 +0900

    시간초과 해결

--- SWEA/D3/5215_햄버거 다이어트.py
+++ SWEA/D3/5215_햄버거 다이어트.py
@@ -1,14 +1,11 @@
-def dfs(score_sum, cal_sum):
+def dfs(score_sum, cal_sum, index):
     global max_score
-    for i in range(N):
-        if cal_sum + ingredient[i][1] > L:
-            if score_sum >= max_score:
-                max_score = score_sum 
-            return
-        if not visit[i] and cal_sum + ingredient[i][1] <= L:
-            visit[i] = 1
-            dfs(score_sum + ingredient[i][0], cal_sum + ingredient[i][1])
-            visit[i] = 0
+    if cal_sum > L:
+        return
+    if score_sum >= max_score:
+        max_score = score_sum
+    for i in range(index,N):
+        dfs(score_sum + ingredient[i][0], cal_sum + ingredient[i][1], i+1)
 
 for t in range(1, int(input())+1):
     N, L = map(int,input().split())
@@ -19,5 +16,5 @@ for t in range(1, int(input())+1):
 
     visit = [0 for _ in range(N)]
     max_score = -99999999
-    dfs(0,0)
+    dfs(0,0,0)
     print('#{} {}'.format(t,max_score))
