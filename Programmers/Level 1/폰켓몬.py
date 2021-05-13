07334d0a08af9298d74e1eb2c8c19b6f36048edb
Song Eun Seok <eunsuk93@naver.com>
2021-05-13 16:21:44 +0900

    210513

---
+++ Programmers/Level 1/폰켓몬.py
@@ -0,0 +1,12 @@
+def solution(nums):
+    numsLength = len(nums) 
+    nums = set(nums)
+    if len(nums) < numsLength // 2:
+        return len(nums)
+    else:
+        return numsLength // 2
+
+if __name__ == "__main__":
+    print(solution([3,1,2,3]))
+    print(solution([3,3,3,2,2,4]))
+    print(solution([3,3,3,2,2,2]))
