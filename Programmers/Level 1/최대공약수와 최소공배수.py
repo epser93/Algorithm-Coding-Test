6649926775f55532f8f5cf26e6f7d66f01fbfaae
Song Eun Seok <eunsuk93@naver.com>
2021-06-01 01:24:27 +0900

    210601 재풀이필요

---
+++ Programmers/Level 1/최대공약수와 최소공배수.py
@@ -0,0 +1,24 @@
+def solution(n, m):
+    minNum = min(n, m)
+    maxNum = max(n, m)
+    gongYak = 1
+    gongBae = 1
+    while 1:
+        flag = False
+        for num in range(minNum, 1, -1):
+            if maxNum % num == 0 and minNum % num == 0:
+                gongYak *= num
+                maxNum //= num
+                minNum //= num
+                flag = True
+                break
+        if flag:
+            continue
+        gongYak *= 1
+        gongBae *= (gongYak * maxNum * minNum)
+        break
+    return [gongYak, gongBae]
+
+if __name__ == "__main__":
+    print(solution(3, 12))
+    print(solution(2, 5))
