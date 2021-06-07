9a319fbafcd2a3dc672d009db1d350e436601810
Song Eun Seok <eunsuk93@naver.com>
2021-06-07 22:13:18 +0900

    210607

--- Programmers/2018_Summer_Winter_Coding/스킬트리.py
+++ Programmers/2018_Summer_Winter_Coding/스킬트리.py
@@ -1,26 +1,16 @@
-def getSkillSystem(skills):
-    skillTree = {}
-    visit = ""
-    for skill in skills:
-        if visit:
-            skillTree[skill] = visit
-        visit = skill
-    return skillTree
+from collections import deque
 
 def solution(skill, skill_trees):
     answer = 0
-    skillSystem = getSkillSystem(skill)
     for skill_tree in skill_trees:
-        visit = []
-        isCorrect = True
-        for skill in skill_tree:
-            if skillSystem.get(skill) and skillSystem.get(skill) not in visit:
-                isCorrect = False
-                break
-            visit.append(skill)
-        if not isCorrect:
-            continue
-        answer += 1
+        skillOrder = deque(skill)
+        for oneSkill in skill_tree:
+            if oneSkill in skill:
+                if skillOrder[0] != oneSkill:
+                    break
+                skillOrder.popleft()
+        else:
+            answer += 1
     return answer
 
 if __name__ == "__main__":
