97fff3b20de8e16826fdadfe94e178c709a6e6c5
Song Eun Seok <eunsuk93@naver.com>
2021-06-07 21:48:42 +0900

    210607

--- Programmers/2018_카카오/캐시.py
+++ Programmers/2018_카카오/캐시.py
@@ -1,24 +1,17 @@
 from collections import deque
 
-def solution(cacheSize, cites):
-    cache = deque(maxlen=cacheSize)
-    time = 0
-    for city in cites:
-        city = city.lower()
-        if city in cache:
-            cache.remove(city)
-            cache.append(city)
-            time += 1
+def solution(skill, skill_trees):
+    answer = 0
+    for skill_tree in skill_trees:
+        skillOrder = deque(skill)
+        for oneSkill in skill_tree:
+            if oneSkill in skill:
+                if skillOrder[0] != oneSkill:
+                    break
+                skillOrder.popleft()
         else:
-            cache.append(city)
-            time += 5
-
-    return time
+            answer += 1
+    return answer
 
 if __name__ == "__main__":
-    print(solution(3, ["Jeju", "Pangyo", "Seoul", "NewYork", "LA", "Jeju", "Pangyo", "Seoul", "NewYork", "LA"]))
-    print(solution(3, ["Jeju", "Pangyo", "Seoul", "Jeju", "Pangyo", "Seoul", "Jeju", "Pangyo", "Seoul"]))
-    print(solution(2, ["Jeju", "Pangyo", "Seoul", "NewYork", "LA", "SanFrancisco", "Seoul", "Rome", "Paris", "Jeju", "NewYork", "Rome"]))
-    print(solution(5, ["Jeju", "Pangyo", "Seoul", "NewYork", "LA", "SanFrancisco", "Seoul", "Rome", "Paris", "Jeju", "NewYork", "Rome"]))
-    print(solution(2, ["Jeju", "Pangyo", "NewYork", "newyork"]))
-    print(solution(0, ["Jeju", "Pangyo", "Seoul", "NewYork", "LA"]))
+    print(solution("CBD", ["BACDE", "CBADF", "AECB", "BDA"]))
