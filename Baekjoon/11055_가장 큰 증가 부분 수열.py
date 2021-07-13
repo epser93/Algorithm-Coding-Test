1cae55e8d23330f93ce675452d39ec5613e41b49
Song Eun Seok <eunsuk93@naver.com>
2021-07-13 13:23:20 +0900

    210713 재풀이필요

---
+++ Baekjoon/11055_가장 큰 증가 부분 수열.py
@@ -0,0 +1,11 @@
+import sys
+
+input = sys.stdin.readline
+N = int(input())
+numbers = list(map(int, input().split()))
+dp = [x for x in numbers]
+for i in range(1, N):
+    for j in range(i-1, -1, -1):
+        if numbers[i] > numbers[j]:
+            dp[i] = max(dp[j] + numbers[i], dp[i])
+print(max(dp))
